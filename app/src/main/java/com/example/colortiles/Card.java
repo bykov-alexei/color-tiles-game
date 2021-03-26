0c1d8d2124510f5e0bbef2521d32bada38ccd204
Alexei <mail@bykov-alexei.ru>
2021-03-26 18:27:14 +0800

    Refactored

--- app/src/main/java/com/example/colortiles/Card.java
+++ app/src/main/java/com/example/colortiles/Card.java
@@ -1,21 +1,23 @@
 package com.example.colortiles;
 
+import android.content.res.Resources;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
 public class Card {
 
-    Paint p = new Paint();
-
+    int color;
+    float x, y, width, height;
+    static List<Integer> colors = Arrays.asList(
+            R.color.tileColorR, R.color.tileColorG,
+            R.color.tileColorB);
 
     public Card(float x, float y, float width, float height, int color) {
         this.color = color;
@@ -25,29 +27,21 @@ public class Card {
         this.height = height;
     }
 
-    int color;
-    float x, y, width, height;
-    int nextColor = 0;
-    List<Integer> colors = Arrays.asList(
-            R.color.tileColorR, R.color.tileColorG,
-            R.color.tileColorB);
-
-
-    public boolean changeColor(float touchX, float touchY) {
-        if (touchX >= x && touchX <= x + width && touchY >= y && touchY <= y + height) {
-            return true;
-        } else return false;
+    public boolean touched(float touchX, float touchY) {
+        return touchX >= x && touchX <= x + width && touchY >= y && touchY <= y + height;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public void draw(Canvas c) {
-        //нарисовать карту в виде прямоугольника
-
-        //p.setColor(colors.get(0));
-        p.setColor(color);
-        //c.drawRect(x, y, x + width, y + height, p);
+    public void draw(Canvas c, Resources res) {
+        Paint p = new Paint();
+        p.setColor(res.getColor(color));
         c.drawRoundRect(x, y, x + width, y + height, 25, 25, p);
     }
 
+    public void nextColor() {
+        int nextColor = colors.indexOf(color);
+        nextColor = (nextColor + 1) % colors.size();
+        color = colors.get(nextColor);
+    }
 
 }
--- app/src/main/java/com/example/colortiles/TilesView.java
+++ app/src/main/java/com/example/colortiles/TilesView.java
@@ -3,12 +3,8 @@ package com.example.colortiles;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
@@ -17,36 +13,15 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 
 public class TilesView extends View {
 
-    final int PAUSE_LENGTH = 1;
     int displayWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-    int openedCards = 0;
-
-    float tmpWidth = displayWidth / 5;
-    float tmpHeight = displayWidth / 5;
-    float tmpX = tmpWidth / 3;
-    float tmpY = tmpWidth / 3;
 
     ArrayList<Card> cards = new ArrayList<>();
-    List<Integer> colors;
-
-    boolean isOnPauseNow = false;
-
-    Card firstCard = null;
-    int counter = 0;
-    int currentIndex;
-    int nextColor;
-
-    int redCount;
-    int blueCount;
-    int greenCount;
 
     public TilesView(Context context) {
         super(context);
@@ -55,7 +30,6 @@ public class TilesView extends View {
     public TilesView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setColorsAndTiles();
-
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -63,188 +37,98 @@ public class TilesView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         for (Card c : cards) {
-            c.draw(canvas);
+            c.draw(canvas, getResources());
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
         int x = (int) event.getX();
         int y = (int) event.getY();
-        //      Log.d("myTag", "onTouchEvent: " + cards.get(3).color);
-        if (event.getAction() == MotionEvent.ACTION_DOWN && !isOnPauseNow) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
             for (Card c : cards) {
-                if (c.changeColor(x, y)) {
+                if (c.touched(x, y)) {
+                    int currentIndex = cards.indexOf(c);
+                    c.nextColor();
+                    affectNear(currentIndex);
 
-                    if (c.color == colors.get(0)) {
-                        nextColor = 1;
-                    }
-                    if (c.color == colors.get(1)) {
-                        nextColor = 2;
-                    }
-                    if (c.color == colors.get(2)) {
-                        nextColor = 0;
-                    }
-                    currentIndex = cards.indexOf(c);
-
-                    c.color = colors.get(nextColor);
-                    redrawLine(currentIndex);
                     invalidate();
-                    PauseTask task = new PauseTask();
-                    task.execute();
+                    if (checkWin()) {
+                        handleWin();
+                    }
                     return true;
-
-
                 }
             }
-
-
         }
-
-
         invalidate();
         return true;
     }
 
-    class PauseTask extends AsyncTask<Integer, Void, Void> {
-        @Override
-        protected Void doInBackground(Integer... integers) {
-
-            return null;
+    public boolean checkWin() {
+        Card firstCard = cards.get(0);
+        int counter = 0;
+        for (int i = 0; i < cards.size(); i++) {
+            if (cards.get(i).color == firstCard.color) counter++;
         }
+        return counter == 16;
+    }
 
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            firstCard = cards.get(0);
-            for (int i = 0; i < cards.size(); i++) {
-                if (cards.get(i).color == firstCard.color) counter++;
-                if (cards.get(i).color == colors.get(0)) redCount ++;
-                if (cards.get(i).color == colors.get(1)) greenCount ++;
-                if (cards.get(i).color == colors.get(2)) blueCount ++;
-            }
-            Log.d("myTag", "onPostExecute: " + counter);
-            if (counter == 16) {
-                Toast toast = Toast.makeText(getContext(),
-                        "Победа!", Toast.LENGTH_SHORT);
-                toast.show();
-                newGame();
-            }
-            counter = 0;
-        }
-
+    public void handleWin() {
+        Toast toast = Toast.makeText(getContext(),
+                "Победа!", Toast.LENGTH_SHORT);
+        toast.show();
+        newGame();
     }
 
 
     public void setColorsAndTiles() {
-        colors = Arrays.asList(
-                getResources().getColor(R.color.tileColorR), getResources().getColor(R.color.tileColorG),
-                getResources().getColor(R.color.tileColorB));
+        float tmpWidth = displayWidth / 5;
+        float tmpHeight = displayWidth / 5;
+        float tmpX = tmpWidth / 3;
+        float tmpY = tmpWidth / 3;
 
-
-        Collections.shuffle(colors);
-        int cnt = 0;
+        List<Integer> colors = Card.colors;
         for (int i = 0; i < 16; i++) {
-
-            cards.add(new Card(tmpX, tmpY, tmpWidth, tmpHeight, colors.get(cnt)));
-            cnt++;
-            if (cnt == 3) {
-                cnt = 0;
+            if (i % 3 == 0) {
                 Collections.shuffle(colors);
             }
+            cards.add(new Card(tmpX, tmpY, tmpWidth, tmpHeight, colors.get(i % 3)));
 
             tmpX += tmpWidth + tmpWidth / 10;
             if (((i + 1) % 4) == 0) {
                 tmpX = tmpWidth / 3;
                 tmpY += tmpWidth + tmpWidth / 10;
             }
-
         }
-        tmpWidth = displayWidth / 5;
-        tmpHeight = displayWidth / 5;
-        tmpX = tmpWidth / 3;
-        tmpY = tmpWidth / 3;
     }
 
 
     public void newGame() {
-
         cards.clear();
         setColorsAndTiles();
         invalidate();
     }
 
-    public void redrawLine(int currentIndex) {
+    public void affectNear(int currentIndex) {
         ArrayList<Integer> nextIndexes = new ArrayList<>();
-        if (currentIndex == 0) {
-            nextIndexes.add(currentIndex + 1);
-            nextIndexes.add(currentIndex + 4);
-
-        }
-
-        if (currentIndex > 0 && currentIndex < 3) {
-            nextIndexes.add(currentIndex + 1);
-            nextIndexes.add(currentIndex - 1);
-            nextIndexes.add(currentIndex + 4);
-        }
-
-        if (currentIndex > 12 && currentIndex < 15) {
-            nextIndexes.add(currentIndex + 1);
-            nextIndexes.add(currentIndex - 1);
-            nextIndexes.add(currentIndex - 4);
-        }
-
-        if (currentIndex == 3) {
-            nextIndexes.add(currentIndex - 1);
-            nextIndexes.add(currentIndex + 4);
-
-        }
-
-        if (currentIndex > 4 && currentIndex < 7 || currentIndex > 8 && currentIndex < 11) {
-            nextIndexes.add(currentIndex + 1);
-            nextIndexes.add(currentIndex - 1);
-            nextIndexes.add(currentIndex + 4);
-            nextIndexes.add(currentIndex - 4);
-        }
-
-        if (currentIndex == 4 || currentIndex == 8) {
-            nextIndexes.add(currentIndex + 4);
-            nextIndexes.add(currentIndex - 4);
-            nextIndexes.add(currentIndex + 1);
-        }
-
-        if (currentIndex == 7 || currentIndex == 11) {
-            nextIndexes.add(currentIndex + 4);
-            nextIndexes.add(currentIndex - 4);
+        int x = currentIndex % 4;
+        int y = currentIndex / 4;
+        if (x - 1 >= 0) {
             nextIndexes.add(currentIndex - 1);
         }
-
-        if (currentIndex == 12) {
+        if (x + 1 < 4) {
             nextIndexes.add(currentIndex + 1);
-            nextIndexes.add(currentIndex - 4);
-
         }
-
-        if (currentIndex == 15) {
-            nextIndexes.add(currentIndex - 1);
+        if (y - 1 >= 0) {
             nextIndexes.add(currentIndex - 4);
-
+        }
+        if (y + 1 < 4) {
+            nextIndexes.add(currentIndex + 4);
         }
 
         for (Integer n : nextIndexes) {
-            if (cards.get(n).color == colors.get(0)) {
-                nextColor = 1;
-            }
-            if (cards.get(n).color == colors.get(1)) {
-                nextColor = 2;
-            }
-            if (cards.get(n).color == colors.get(2)) {
-                nextColor = 0;
-            }
-            cards.get(n).color = colors.get(nextColor);
+            cards.get(n).nextColor();
         }
-
         nextIndexes.clear();
     }
 
